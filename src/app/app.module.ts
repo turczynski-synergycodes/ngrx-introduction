f600a5f306019c1f5d0d0a66a11153542225b1b5
Przemysław Turczyński <przemyslaw.turczynski@synergycodes.com>
2021-06-19 19:06:41 +0200

    Add ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BaseModule } from './base/base.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
 import { CartIconContainerModule } from './features/cart-icon/containers/cart-icon-container/cart-icon-container.module';
 import { productsReducer } from './state/products/products.reducer';
 import { cartItemsReducer } from './state/cart-items/cart-items.reducer';
+import { BaseModule } from './base/base.module';
+import { environment } from './../environments/environment';
 
 @NgModule({
     declarations: [
@@ -25,7 +27,8 @@ import { cartItemsReducer } from './state/cart-items/cart-items.reducer';
         StoreModule.forRoot({
             productsState: productsReducer,
             cartItemsState: cartItemsReducer
-        })
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production})
     ],
     providers: [],
     bootstrap: [AppComponent],
